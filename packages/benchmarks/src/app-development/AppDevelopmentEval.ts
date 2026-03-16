b4b160641b8f1738dbbbd9303096e829fdeac121
Ben Perlmutter <ben.p@mongodb.com>
2026-03-16 13:23:27 -0400

    create dataset

---
+++ packages/benchmarks/src/app-development/AppDevelopmentEval.ts
@@ -0,0 +1,52 @@
+import {
+  EvalCase,
+  EvalParameters,
+  EvalScorer,
+  EvalTask,
+} from "mongodb-rag-core/braintrust";
+import { ConversationEvalCase } from "mongodb-rag-core/eval";
+import { LlmOptions } from "mongodb-rag-core/executeCode";
+
+export type AppDevelopmentEvalCaseInput = {
+  name: string;
+  messages: ConversationEvalCase["messages"];
+};
+
+export type AppDevelopmentTag = string;
+
+export type AppDevelopmentMetadata = Record<string, unknown> &
+  Partial<Omit<LlmOptions, "openAiClient">> & {
+    difficulty: "beginner" | "intermediate" | "advanced";
+    is_mongodb_optimal?: boolean;
+    category?: string;
+  };
+
+export interface AppDevelopmentEvalCase
+  extends EvalCase<
+    AppDevelopmentEvalCaseInput,
+    AppDevelopmentTaskExpected,
+    AppDevelopmentMetadata
+  > {
+  tags: AppDevelopmentTag[];
+}
+
+export type AppDevelopmentTaskOutput = {
+  response: string;
+};
+
+export type AppDevelopmentTaskExpected = void;
+
+export type AppDevelopmentEvalTask = EvalTask<
+  AppDevelopmentEvalCaseInput,
+  AppDevelopmentTaskOutput,
+  AppDevelopmentTaskExpected,
+  AppDevelopmentMetadata,
+  EvalParameters
+>;
+
+export type AppDevelopmentEvalScorer = EvalScorer<
+  AppDevelopmentEvalCaseInput,
+  AppDevelopmentTaskOutput,
+  AppDevelopmentTaskExpected,
+  AppDevelopmentMetadata
+>;
